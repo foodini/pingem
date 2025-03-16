fbc59cab366c1f4f9b15d9003bc177f092c4de00
Ron <foodini@gmail.com>
2025-03-15 19:08:41 -0700

    Massive rewrite to eliminate the python backend.
    There is still _a_ backend, and unfortunately it has to be run as root, but I
    will get it rewritten entirely in C/++ so it can be compiled and setuid root.
    This is a bit of a departure from the original intention of this project, but
    with ICMP still languishing in the 1980s, there really wasn't much of a choice.

--- user_icmp2.py
+++ user_icmp2.py
@@ -7,15 +7,15 @@
 # * handle Destination Net Unreachable.
 
 #TODO, Medium Priority:
-# * fileno isn't quite enough to ID a packet. What if there are two instances
-#   of user_icmp running at the same time? Each could have a caller on fileno=5
+# * connection_id isn't quite enough to ID a packet. What if there are two instances
+#   of user_icmp running at the same time? Each could have a caller on connection_id=5
 #   and the two instance would confuse the other's responses as their own.
 # * need to consider defense against abusers. I have a brief delay before
 #   sending starts, but I should keep a token bucket for each user and allow
 #   admin to set the refil rate and count.
 
 # TODO: Low Priority:
-# * Should I stop sending fileno over the network? Writting a mapper just adds
+# * Should I stop sending connection_id over the network? Writting a mapper just adds
 #   more code that has to run in privileged mode.
 # * Do I need a defense against bursting? If 100 targets are added at the same
 #   time with the same interval, they're going to tend to affect each other.
@@ -25,6 +25,7 @@
 #   Given that the current minimum packet size is 34, it's hard to justify.
 
 from collections import defaultdict
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 import os
 from queue import PriorityQueue, Queue
 import random
@@ -33,54 +34,53 @@ import socket
 import struct
 import threading
 import time
+from urllib.parse import urlparse, parse_qs
 
 MAX_PACKET_SIZE = 8192
 MAGIC_STRING = bytes('867-5309', 'utf-8')
 MAGIC_STRING_LEN = len(MAGIC_STRING)
 
-#TODO: make Ping and Pong derive from a common ancestor
-
 class ICMP():
   def to_dotted_ip(self, long):
     return f'{(long>>24) % 256}.{(long>>16) % 256}.{(long>>8) % 256}.{(long) % 256}'
 
-  def to_bytes(self):
-    return(bytes(self.str(), 'utf-8'))
-
 class Ping(ICMP):
-  def __init__(self, fileno, icmp_seq, timestamp):
-    self.fileno = fileno
+  def __init__(self, resolved_address, connection_id, icmp_seq, timestamp):
+    self.resolved_address = resolved_address
+    self.connection_id = connection_id
     self.icmp_seq = icmp_seq
     self.timestamp = timestamp
 
-  def str(self):
-    return f'S {self.fileno} {self.icmp_seq} {self.timestamp}\n'
+  def __str__(self):
+    return('{' + f'"type":"ping", "to":"{self.resolved_address}", "icmp_seq":"{self.icmp_seq}", ' +
+           f'"timestamp":"{self.timestamp}"' + '}')
 
 class Pong(ICMP):
-  def __init__(self, fileno, icmp_seq, timestamp, rtt, source_ip, ttl):
-    self.fileno = fileno
+  def __init__(self, connection_id, icmp_seq, timestamp, rtt, source_ip, ttl):
+    self.connection_id = connection_id
     self.icmp_seq = icmp_seq
     self.timestamp = timestamp
     self.rtt = rtt
     self.source_ip = source_ip
     self.ttl = ttl
 
-  def str(self):
+  def __str__(self):
     dotted_ip = self.to_dotted_ip(self.source_ip)
-    return f'R {self.fileno} {self.icmp_seq} {self.timestamp} {self.rtt} {dotted_ip} {self.ttl}\n'
+    return ('{' + f'"type":"reply", "from":"{dotted_ip}", "icmp_seq":"{self.icmp_seq}", ' +
+            f'"timestamp":"{self.timestamp}", "rtt":"{self.rtt}", "ttl":"{self.ttl}"' + '}')
 
 #TODO: derive a Target type for handling traceroutes
 class Target():
-  def __init__(self, unresolved_address, resolved_address, fileno, min_packet_size, interval):
+  def __init__(self, unresolved_address, resolved_address, connection_id, packet_size, interval):
     self.unresolved_address = unresolved_address
     self.resolved_address = resolved_address
-    self.fileno = fileno
-    self.min_packet_size = min_packet_size
+    self.connection_id = connection_id
+    self.packet_size = packet_size
     self.interval = interval
     self.icmp_seq = 1
 
   def __str__(self):
-    return f'{self.unresolved_address} {self.resolved_address} fd:{self.fileno}'
+    return f'{self.unresolved_address} {self.resolved_address} fd:{self.connection_id}'
 
   def bump(self):
     self.next_ping_time += self.interval
@@ -91,7 +91,7 @@ class SocketManager():
     self.raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     self.ip_header_size = 20
     self.icmp_header_size = 8
-    # magic, fileno, icmp_seq, timestamp
+    # magic, connection_id, icmp_seq, timestamp
     self.user_ping_header_size = MAGIC_STRING_LEN + 2 + 8 + 8
     self.all_header_size = self.user_ping_header_size + self.icmp_header_size + self.ip_header_size
 
@@ -107,13 +107,13 @@ class SocketManager():
     checksum = ~checksum & 0xFFFF
     return checksum
 
-  def build_icmp(self, payload, fileno, icmp_seq):
+  def build_icmp(self, payload, connection_id, icmp_seq):
     now = time.time()
     icmp_packet = (struct.pack(
         f'!BBHHH{MAGIC_STRING_LEN}sHQd',
         #type, code, checksum, identifier, seq number
         8, 0, 0, 12345, icmp_seq % 65536,
-        MAGIC_STRING, fileno, icmp_seq, now)
+        MAGIC_STRING, connection_id, icmp_seq, now)
         + payload
     )
     c = self.calc_checksum(icmp_packet)
@@ -121,7 +121,7 @@ class SocketManager():
         f'!BBHHH{MAGIC_STRING_LEN}sHQd',
         #type, code, checksum, identifier, seq number
         8, 0, c, 12345, icmp_seq % 65536,
-        MAGIC_STRING, fileno, icmp_seq, now)
+        MAGIC_STRING, connection_id, icmp_seq, now)
         + payload
     )
 
@@ -129,13 +129,13 @@ class SocketManager():
 
   #TODO: if "destination" is an ip name with multiple associated ip addresses (like amazon.com),
   #      sendto will arbitrarily choose between them with each packet. Kinda need to fix this.
-  def send(self, destination, fileno, icmp_seq, length):
+  def send(self, resolved_address, connection_id, icmp_seq, length):
     # TODO: don't build the pad every time. Stash it in the target or something.
     payload = bytes('x', 'utf-8') * (length - self.icmp_header_size - self.user_ping_header_size)
-    packet, timestamp = self.build_icmp(payload, fileno, icmp_seq)
-    self.raw_socket.sendto(packet, (destination, 0))
+    packet, timestamp = self.build_icmp(payload, connection_id, icmp_seq)
+    self.raw_socket.sendto(packet, (resolved_address, 0))
 
-    return Ping(fileno, icmp_seq, timestamp)
+    return Ping(resolved_address, connection_id, icmp_seq, timestamp)
 
   def tick(self, timeout_time):
     try:
@@ -154,7 +154,7 @@ class SocketManager():
             icmp_type, code, icmp_checksum,
             identifier, _icmp_seq,
             #end of ICMP header
-            magic_string, fileno, icmp_seq, timestamp
+            magic_string, connection_id, icmp_seq, timestamp
         ) = struct.unpack(
             '!'
             'BBH'
@@ -170,13 +170,14 @@ class SocketManager():
             response[:self.all_header_size]
         )
         if magic_string == MAGIC_STRING:
-          return Pong(fileno, icmp_seq, timestamp, now-timestamp, source_addr, ttl)
+          return Pong(connection_id, icmp_seq, timestamp, now-timestamp, source_addr, ttl)
         else:
           print('.', end='', flush=True)
           return None
     except socket.error as error:
       #TODO: this is probably fatal. Shut down gracefully. Though I've only ever seen it when ^C.
       print(f'Erorr: {error}')
+
 
 class QueueManager():
   def __init__(self, socket_manager):
@@ -198,7 +199,7 @@ class QueueManager():
       pong = self.socket_manager.tick(1)
       if pong:
         # For some reason, tick is occasionally rejecting
-        self.events[pong.fileno].put(pong)
+        self.events[pong.connection_id].put(pong)
 
   def sender(self):
     print('sender running...')
@@ -219,42 +220,45 @@ class QueueManager():
       try:
         #TODO: deal with the race condition: Can a target still be queued after requestor has killed it?
         ping = self.socket_manager.send(
-            next_target.resolved_address, next_target.fileno,
-            next_target.icmp_seq, next_target.min_packet_size)
+            next_target.resolved_address, next_target.connection_id,
+            next_target.icmp_seq, next_target.packet_size)
       except OSError as e:
         # "Network is Unreachable". If that's the case, report that the packet was sent. This isn't
         # true, of course, but the way a user will experience it is as an inability to traverse the
         # network.
-        ping = Ping(next_target.fileno, next_target.icmp_seq, time.time())
+        ping = Ping(next_target.resolved_address, next_target.connection_id, next_target.icmp_seq,
+                    time.time())
 
-      self.events[next_target.fileno].put(ping)
+      self.events[next_target.connection_id].put(ping)
       next_target.bump()
       # There's a chance that this target has been removed since we pulled it from the queue, so
       # we need to see if it still exists before we requeue it.
-      if next_target.fileno in self.targets:
+      if next_target.connection_id in self.targets:
         with self.queue_lock:
           self.queue.put((next_target.next_ping_time, next_target))
 
-  def get_event(self, fileno):
-    return self.events[fileno].get()
+  def get_event(self, connection_id):
+    return self.events[connection_id].get()
 
-  def add_target(self, unresolved_address, resolved_address, fileno, min_packet_size, interval):
-    target = Target(unresolved_address, resolved_address, fileno, min_packet_size, interval)
+  def add_target(self, address='8.8.8.8', resolved_address='8.8.8.8', packet_size=64, interval=1.0,
+                 connection_id=-1):
+    print('add_target called w/ interval=', interval)
+    target = Target(address, resolved_address, connection_id, packet_size, interval)
     next_ping_time = time.time() + 0.01
     target.next_ping_time = next_ping_time
     with self.queue_lock:
-      self.targets[fileno] = target
+      self.targets[connection_id] = target
       self.queue.put((next_ping_time, target))
 
-  def remove_target(self, fileno):
-    #I can't just store a fileno and ignore it when I come across it - that fileno may be reused.
+  def remove_target(self, connection_id):
+    #I can't just store a connection_id and ignore it when I come across it - that connection_id may be reused.
     #TODO: is there a better way?
     removed_targets = []
     with self.queue_lock:
       new_pq = PriorityQueue()
       while(len(self.queue.queue)):
         prio, target = self.queue.get()
-        if target.fileno == fileno:
+        if target.connection_id == connection_id:
           removed_targets.append(target)
         else:
           new_pq.put((prio, target))
@@ -262,100 +266,92 @@ class QueueManager():
 
       new_targets = {}
       for k,v in self.targets.items():
-        if k != fileno:
+        if k != connection_id:
           new_targets[k] = v
       self.targets = new_targets
     return removed_targets
+
 
 SOCKET_PATH = '/tmp/user_icmp_socket'
 if os.path.exists(SOCKET_PATH):
   os.remove(SOCKET_PATH)
 
-#TODO: log requests, especially abuses (like interval less than 0.005)
-def get_client_uid(connection):
-  try:
-    fmt = 'i i i'
-    ucred = connection.getsockopt(socket.SOL_SOCKET, socket.SO_PEERCRED, struct.calcsize(fmt))
-    pid, uid, gid = struct.unpack(fmt, ucred)
-    return uid
-  except OSError as e:
-    print(f'Error getting client credentials: {e}')
-    return None
+socket_manager = SocketManager()
+queue_manager = QueueManager(socket_manager)
 
-class ClientHandler():
-  #TODO: unicode characters. Limit interval to one decimal point.
-  ping_start_command_re = r'^P [a-zA-Z0-9.-]+ [0-9.]+ [0-9]+$'
+next_unique_id = -1
+def get_next_unique_id():
+  global next_unique_id
 
-  def __init__(self):
-    self.server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    self.server_socket.bind(SOCKET_PATH)
-    os.chmod(SOCKET_PATH, 0o777)
-    self.server_socket.listen(1) #TODO: does the backlog (currently 1) need to be increased?
+  next_unique_id += 1
+  return next_unique_id
 
-    self.socket_manager = SocketManager()
-    self.queue_manager = QueueManager(self.socket_manager)
 
-  def handle_command(self, connection):
-    try:
-      command = connection.recv(8192)
-      if(len(command) < 5 or len(command) > 8180):
-        #client has disconnected, or they're sending something weird and we're hanging up.
-        return False
-    except TimeoutError as to:
-      return True
+class RequestHandler(BaseHTTPRequestHandler):
+  def encode_as_wire_message(self, data):
+    return bytes('data: ' + str(data) + '\n\n', 'utf8')
 
-    command = command.decode()
-    if not re.match(ClientHandler.ping_start_command_re, command):
-      return False
+  def do_GET(self):
+    parsed_url = urlparse(self.path)
+    query_params = parse_qs(parsed_url.query)
 
-    _, unresolved_address, interval, size = command.split()
-    interval = max(float(interval), 0.01)
-    size = min(int(size), 8192)
-    try:
-      resolved_address = socket.gethostbyname(unresolved_address)
-      self.queue_manager.add_target(
-          unresolved_address, resolved_address, connection.fileno(), size, interval)
-    except socket.gaierror as e:
-      connection.sendall(bytes(f'E {unresolved_address} {e}\n'))
-      return False
+    for k,v in query_params.items():
+      query_params[k] = v[0]
 
-    return True
+    #TODO: ensure that the request comes from localhost. Figure out who the user
+    #      is and log the request. Also, occasionally log their usage and when
+    #      they disconnect, log a summary.
 
-  def handle_client_connection(self, connection):
-    try:
-      if not self.handle_command(connection):
-        return
+    endpoint = {
+      'address': '8.8.8.8',
+      'interval': '1.0',
+      'packet_size': '64',
+    }
 
-      while True:
-        #check for messages, pass them to the user:
-        event = self.queue_manager.get_event(connection.fileno())
-        if not event:
-          return
-        connection.sendall(event.to_bytes())
+    endpoint.update(query_params)
+    keys = list(endpoint.keys())
+    keys.sort()
 
-    except BrokenPipeError as e:
-      print(f'BrokenPipeError (probably, user closed connection) on fileno {connection.fileno()}')
-      pass
-    finally:
-      victims = self.queue_manager.remove_target(connection.fileno())
-      print(f'closing connection for fileno {connection.fileno()}...')
-      connection.close()
+    if keys != ['address', 'interval', 'packet_size']:
+      self.send_response(404)
+      self.end_headers()
+      self.finish()
+      self.connection.close()
+      return
 
-  def run(self):
-    try:
-      while True:
-        connection, _ = self.server_socket.accept()
-        uid = get_client_uid(connection)
-        print(f'new connection from user {uid}, fileno: {connection.fileno()}')
-        t = threading.Thread(target=self.handle_client_connection, args=(connection,))
-        t.start()
+    endpoint['interval'] = float(endpoint['interval'])
+    endpoint['packet_size'] = int(endpoint['packet_size'])
+    endpoint['resolved_address'] = socket.gethostbyname(endpoint['address'])
+    endpoint['connection_id'] = get_next_unique_id()
+    print('endpoint created:', endpoint)
+    queue_manager.add_target(**endpoint)
 
-    finally:
-      print('closing socket...')
-      self.server_socket.close()
-      print('socket closed')
-      #os.remove(SOCKET_PATH)
-      print('socket path removed')
+    self.send_response(200)
+    self.send_header('Content-type', 'text/event-stream')
+    self.send_header('Cache-Control', 'no-cache')
+    self.send_header('Connection', 'keep-alive')
+    self.send_header('Access-Control-Allow-Credentials', 'true')
+    self.send_header('Access-Control-Allow-Origin', '*')
+    self.end_headers()
 
-client_handler = ClientHandler()
-client_handler.run()
+    while True:
+      event = queue_manager.get_event(endpoint['connection_id'])
+      wire_message = self.encode_as_wire_message(str(event))
+      self.wfile.write(wire_message)
+
+# UNDER NO CIRCUMSTANCES WILL THIS SURVIVE PAST THE PROTOTYPE PHASE. I don't
+# trust the python httpd to serve traffic as root. When this gets ported to C,
+# it'll be a bespoke solution to serve ONLY very simple HTTP.
+def serve_requests(server_class=ThreadingHTTPServer, handler_class=RequestHandler):
+  #TODO: make port user-configurable
+  server_address = ('', 8000)
+  httpd = server_class(server_address, handler_class)
+  
+  print('starting httpd...')
+  httpd.serve_forever()
+
+
+serve_requests()
+print('after server_requests')
+
+
